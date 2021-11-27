9b3c11e3faa7ffad36d941ab1e7cc510fc5c7110
Nathan Stone <nathan.stone@d-techinternational.com>
2021-11-27 23:40:19 +0000

    Added Cart class

---
+++ PromotionEngine/Cart.cs
@@ -0,0 +1,16 @@
+﻿namespace PromotionEngine
+{
+    public class Cart
+    {
+        public IList<Product> Items { get; } = new List<Product>();
+
+        public IList<IPromotion> AppliedPromotions { get; } = new List<IPromotion>();
+
+        public decimal CalculateTotal()
+        {
+            var itemsTotal = Items.Sum(x => x.Price);
+            var reduction = AppliedPromotions.Sum(x => x.CalculateReduction(Items));
+            return itemsTotal - reduction;
+        }
+    }
+}
