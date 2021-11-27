5dfb92937d82b45b480499869964d074e8a7cfa2
Nathan Stone <nathan.stone@d-techinternational.com>
2021-11-27 23:33:25 +0000

    Added Product class

---
+++ PromotionEngine/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace PromotionEngine
+{
+    public class Product
+    {
+        public string Id { get; }
+
+        public decimal Price { get; }
+
+        public Product(string id, decimal price)
+        {
+            Id = id;
+            Price = price;
+        }
+    }
+}
