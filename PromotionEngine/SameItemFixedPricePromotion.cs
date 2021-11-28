359181f621646cf2b6b889e69b48fc01554a371d
Nathan Stone <nathan.stone@d-techinternational.com>
2021-11-28 00:42:07 +0000

    Added SameItemFixedPricePromotion class

---
+++ PromotionEngine/SameItemFixedPricePromotion.cs
@@ -0,0 +1,39 @@
+﻿namespace PromotionEngine
+{
+    public class SameItemFixedPricePromotion : IPromotion
+    {
+        public string ProductId { get; }
+        public int CountRequired { get; }
+        public decimal FixedPrice { get; }
+
+        public SameItemFixedPricePromotion(string productId, int countRequired, decimal fixedPrice)
+        {
+            ProductId = productId;
+            CountRequired = countRequired;
+            FixedPrice = fixedPrice;
+        }
+
+        public bool IsSatisfied(IEnumerable<Product> products)
+        {
+            var numberOfApplicableItems = products.Count(x => x.Id == ProductId);
+            return numberOfApplicableItems >= CountRequired;
+        }
+
+        public decimal CalculateReduction(IEnumerable<Product> products)
+        {
+            var applicableItems = products.Where(x => x.Id == ProductId).ToList();
+
+            var numberOfApplicableItems = applicableItems.Count(x => x.Id == ProductId);
+
+            if (numberOfApplicableItems < CountRequired)
+                return 0;
+
+            var promotionApplyCount = (int)Math.Floor((double)numberOfApplicableItems / CountRequired);
+            var itemTotal = applicableItems.Sum(x => x.Price);
+            var itemsNotApplicable = applicableItems.Take(numberOfApplicableItems % CountRequired);
+            var itemsNotApplicableTotal = itemsNotApplicable.Sum(x => x.Price);
+
+            return itemTotal - itemsNotApplicableTotal - (promotionApplyCount * FixedPrice);
+        }
+    }
+}
